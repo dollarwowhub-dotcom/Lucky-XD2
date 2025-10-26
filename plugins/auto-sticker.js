6f37846359754e6c29f84f391a43e7efdcf3083b
dollarwowhub-dotcom <dollarwowhub@gmail.com>
2025-10-26 14:51:38 +0500

    Update auto-sticker.js

--- plugins/auto-sticker.js
+++ plugins/auto-sticker.js
@@ -20,7 +20,7 @@ async (conn, mek, m, { from, body }) => {
 
                     await conn.sendMessage(from, {
                         sticker: stickerBuffer,
-                        packname: 'LUCKY-XD',
+                        packname: 'DOLLAR WOW HUB',
                         author: 'Lucky 218'
                     }, { quoted: mek });
                 } else {
